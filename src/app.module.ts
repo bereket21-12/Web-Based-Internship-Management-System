03205787ecdd32f6534ef14465500d4c14b3f365
Abel <abelzeleke5173@gmail.com>
2024-02-15 23:12:24 +0300

    updated folder structure and model creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,15 @@ import { UsersModule } from './users/users.module';
 // import { PrismaService } from './prisma/prisma.service';
 import { UsersController } from './users/users.controller';
 // import { PrismaModule } from './prisma/prisma.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './common/prisma/prisma.module';
 import { InternshipModule } from './internship/internship.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, InternshipModule, AuthModule, CommonModule],
+  imports: [UsersModule, PrismaModule, InternshipModule, AuthModule, CommonModule, ChatModule],
   controllers: [UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
-// import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
 import { CreateModule } from './create/create.module';
 import { RemoveModule } from './remove/remove.module';
 import { EditModule } from './edit/edit.module';
@@ -10,8 +7,7 @@ import { GetModule } from './get/get.module';
 
 @Module({
   imports: [PrismaModule, CreateModule, RemoveModule, EditModule, GetModule],
-  controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService]
+  controllers: [],
+  providers: [],
 })
-export class UsersModule {}
+export class UsersModule { }
