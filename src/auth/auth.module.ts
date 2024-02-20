cade953ced23185bd690c9ab2113c9c7852b78cf
Abel <abelzeleke5173@gmail.com>
2024-02-20 10:10:10 +0300

    made some changes in the prisma service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,9 @@ import { LoginModule } from './login/login.module';
 import { LogoutModule } from './logout/logout.module';
 import { RegisterModule } from './register/register.module';
 import { SetPermissionModule } from './set-permission/set-permission.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [LoginModule, LogoutModule, RegisterModule, SetPermissionModule]
+  imports: [LoginModule, LogoutModule, RegisterModule, SetPermissionModule, JwtModule]
 })
 export class AuthModule {}
