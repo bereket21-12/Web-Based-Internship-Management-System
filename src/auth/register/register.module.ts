b2e4fa3c0eea9cad447748d96766af01a2f03421
Abel <abelzeleke5173@gmail.com>
2024-02-20 23:05:53 +0300

    University Registration Complete

--- src/auth/register/register.module.ts
+++ src/auth/register/register.module.ts
@@ -3,9 +3,12 @@ import { RegisterService } from './register.service';
 import { RegisterController } from './register.controller';
 import { AtStrategy, RtStrategy } from '../strategies';
 import { GenerateJwtService } from '../jwt/generate.jwt.service';
+import { GenerateJwtModule } from '../jwt/generate.jwt.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [RegisterService, AtStrategy, RtStrategy, GenerateJwtService],
+  imports: [GenerateJwtModule],
+  providers: [RegisterService, AtStrategy, RtStrategy, GenerateJwtService, JwtService],
   controllers: [RegisterController]
 })
 export class RegisterModule { }
