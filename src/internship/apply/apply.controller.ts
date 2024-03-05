44b59cca00c47260fe9a8d92a2f76bca6219f322
Abel <abelzeleke5173@gmail.com>
2024-03-05 16:43:03 +0300

    finished the structural implementation

--- src/internship/apply/apply.controller.ts
+++ src/internship/apply/apply.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { ApplyService } from './apply.service';
+import { CreateApplicationDto } from 'src/common/dtos';
 
 @Controller('apply')
-export class ApplyController {}
+export class ApplyController {
+    constructor(
+        private applyService: ApplyService
+    ) {}
+    
+    @Get()
+    async getApplications() {
+        return this.applyService.getApplications();
+    }
+
+    @Get(':id')
+    async getApplicationById(id: string) {
+        return this.applyService.getApplicationById(id);
+    }
+
+    @Post()
+    async createApplication(@Body() dto: CreateApplicationDto) {
+        return await this.applyService.createApplication(dto);
+    }
+
+    @Patch(':id')
+    async updateApplication(@Body() dto: CreateApplicationDto, id: string) {
+        return await this.applyService.updateApplication(id, dto);
+    }
+
+    @Delete(':id')
+    async deleteApplication(id: string) {
+        return await this.applyService.deleteApplication(id);
+    }
+}
