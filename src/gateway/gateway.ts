40169b783cc1d5bb79705b627327962f9cff5bc4
bereket <berekettadelemulu210@gmail.com>
2024-03-25 10:38:01 +0300

    some updates on socket gateway

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,33 +1,56 @@
-import { OnModuleInit } from "@nestjs/common";
-import { WebSocketGateway ,MessageBody ,SubscribeMessage, WebSocketServer } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { Logger } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class Mygateway implements OnModuleInit {
+@WebSocketGateway({ cors: { origin: 'http://localhost:3000' } })
+export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('SocketGateway');
 
-    @WebSocketServer()
-    server : Server
+  afterInit(server: Server) {
+    this.logger.log('Socket initialized');
+  }
 
-    onModuleInit() {
-        this.server.on('connection',(socket)=>{
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
 
-            console.log(socket.id)
-            console.log('connected')
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
-        })
+  @SubscribeMessage('setup')
+  handleSetup(client: Socket, userData: any): void {
+    client.join(userData._id);
+    client.emit('connected');
+  }
+
+  @SubscribeMessage('join chat')
+  handleJoinChat(client: Socket, room: string): void {
+    client.join(room);
+    this.logger.log(`User joined room: ${room}`);
+  }
+
+  @SubscribeMessage('typing')
+  handleTyping(client: Socket, room: string): void {
+    client.to(room).emit('typing');
+  }
+
+  @SubscribeMessage('stop typing')
+  handleStopTyping(client: Socket, room: string): void {
+    client.to(room).emit('stop typing');
+  }
+
+  @SubscribeMessage('new message')
+  handleNewMessage(client: Socket, newMessageReceived: any): void {
+    const chat = newMessageReceived.chat;
+    if (!chat.users) {
+      this.logger.error('chat.users not defined');
+      return;
     }
-
-
-    @SubscribeMessage('newmessage')
-    onNewMessage(@MessageBody() body:any){
-
-        console.log(body)
-        this.server.emit('onMessage',()=>{
-
-            body
-        })
-
-    }
-
-
+    chat.users.forEach((user: any) => {
+      if (user._id === newMessageReceived.sender._id) return;
+      client.to(user._id).emit('message received', newMessageReceived);
+    });
+  }
 }
